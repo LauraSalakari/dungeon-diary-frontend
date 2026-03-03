3dba4c0eeab0a041f10d13936a02703b41e0d416
Laura <70233672+LauraSalakari@users.noreply.github.com>
2026-03-03 21:57:27 +0100

    improve campaign selector

--- src/nav/CampaignSelector.tsx
+++ src/nav/CampaignSelector.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react'
 import {useAuth} from "../auth/AuthContext.tsx"
-import {FormControl, InputLabel, MenuItem, Select, type SelectChangeEvent} from "@mui/material"
+import {FormControl, MenuItem, Select, type SelectChangeEvent} from "@mui/material"
 import {useCampaign} from "../campaign/CampaignContext.tsx"
 
 export const CampaignSelector: React.FC = () => {
-    const [selectedCampaign, setSelectedCampaign] = React.useState<string>("")
     const auth = useAuth()
     const campaigns = auth?.info?.campaigns
     const campaignContext = useCampaign()
+    const [selectedCampaign, setSelectedCampaign] = React.useState<string>(campaignContext?.campaign?.id ||"")
+
 
     const handleChange = (e: SelectChangeEvent) => {
         e.preventDefault()
@@ -24,8 +25,16 @@ export const CampaignSelector: React.FC = () => {
     return (
         <div style={{width: 300, marginRight:16}}>
             <FormControl fullWidth>
-                <InputLabel id="campaign-selector-label">Select Campaign</InputLabel>
-                <Select label={"Campaign"} id="campaign-selector-label" value={selectedCampaign} onChange={handleChange}>
+                <Select
+                    id="campaign-selector-label"
+                    value={selectedCampaign}
+                    onChange={handleChange}
+                    displayEmpty
+                    inputProps={{ 'aria-label': 'Without label' }}
+                >
+                    <MenuItem value={""}>
+                        <em>Select Campaign</em>
+                    </MenuItem>
                     {campaigns?.map(c =>{
                         return <MenuItem value={c.id} >{c.name}</MenuItem>
                     })}
