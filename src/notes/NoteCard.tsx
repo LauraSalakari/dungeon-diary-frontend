b2f6ffa045bbe30ccddcc947c44077b021f8a1b1
Laura <70233672+LauraSalakari@users.noreply.github.com>
2026-03-06 21:30:05 +0100

    note card color fix

--- src/notes/NoteCard.tsx
+++ src/notes/NoteCard.tsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import {useTheme} from "@mui/material"
 
 interface NoteCardProps {
     content: string
@@ -7,21 +8,24 @@ interface NoteCardProps {
 
 export const NoteCard: React.FC<NoteCardProps> = (props) => {
     const {content, isPrivate} = props
+    const theme = useTheme()
+    const primary = theme.palette.primary.main
+    const secondary = theme.palette.text.secondary
 
     return (
         <div
             style={{
-                border: "1px solid lightgrey",
+                border: `1px solid ${secondary}`,
                 borderRadius: 8,
                 padding: 8,
-                boxShadow: "3px 3px 10px 1px #7a797c",
+                boxShadow: `1px 1px 5px 1px ${primary}`,
             }}
 
         >
             {content}
             <div style={{
                 fontStyle: "italic",
-                color: "lightgray",
+                color: secondary || "lightgray",
                 paddingTop: 4,
             }}>{isPrivate ? "Private note" : "Public note"}</div>
         </div>
